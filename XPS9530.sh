3f1fe4cc89ecefed683550d6851998d368a21297
the-darkvoid <the-darkvoid@users.noreply.github.com>
2014-12-27 09:35:01 +0400

    Added git master update & enhanced output

--- XPS9530.sh
+++ XPS9530.sh
@@ -3,6 +3,8 @@
 # Bold / Non-bold
 BOLD="\033[1m"
 RED="\033[0;31m"
+GREEN="\033[0;32m"
+BLUE="\033[1;34m"
 #echo -e "\033[0;32mCOLOR_GREEN\t\033[1;32mCOLOR_LIGHT_GREEN"
 OFF="\033[m"
 
@@ -12,20 +14,26 @@ GIT_DIR="${REPO}"
 
 git_update()
 {
-	echo "Initializing Laptop-DSDT-Patch & ssdtPRgen"
+	cd ${REPO}
+	echo "${GREEN}[GIT]${OFF}: Updating local data to latest version"
+	
+	echo "${BLUE}[GIT]${OFF}: Updating to latest XPS9530-OSX git master"
+	git pull
+	
+	echo "${BLUE}[GIT]${OFF}: Initializing Laptop-DSDT-Patch & ssdtPRgen"
 	git submodule update --init --recursive
 	
-	echo "Updating Laptop-DSDT-Patch & ssdtPRgen"
+	echo "${BLUE}[GIT]${OFF}: Updating Laptop-DSDT-Patch & ssdtPRgen"
 	git submodule foreach git pull origin master
 }
 
 decompile_dsdt() 
 {
-	echo "Decompiling DSDT / SSDT in ./DSDT/raw"
+	echo "${GREEN}[DSDT]${OFF}: Decompiling DSDT / SSDT in ./DSDT/raw"
 	cd "${REPO}"
 
 	./tools/iasl -w1 -da ./DSDT/raw/DSDT.aml ./DSDT/raw/SSDT-*.aml &> ./logs/dsdt_decompile.log
-	echo "Log created in ./logs/dsdt_decompile.log"
+	echo "${BLUE}[DSDT]${OFF}: Log created in ./logs/dsdt_decompile.log"
 	rm ./DSDT/decompiled/*
 	cp ./DSDT/raw/DSDT.dsl ./DSDT/decompiled/
 	cp ./DSDT/raw/SSDT-1[0235].dsl ./DSDT/decompiled/
@@ -33,7 +41,9 @@ decompile_dsdt()
 
 patch_dsdt()
 {
-	echo "${RED}Patching DSDT in ./DSDT/decompiled${OFF}"
+	echo "${GREEN}[DSDT]${OFF}: Patching DSDT / SSDT"
+	
+	echo "${BLUE}[DSDT]${OFF}: Patching DSDT in ./DSDT/decompiled"
 	
 	echo "${BOLD}[syn] Fix PARSEOP_ZERO Error${OFF}"
 	./tools/patchmatic ./DSDT/decompiled/DSDT.dsl ./externals/Laptop-DSDT-Patch/syntax/fix_PARSEOP_ZERO.txt ./DSDT/decompiled/DSDT.dsl
@@ -98,7 +108,7 @@ patch_dsdt()
 	# SSDT-10 Patches
 	########################
 	
-	echo "${RED}Patching SSDT-10 in ./DSDT/decompiled${OFF}"	
+	echo "${BLUE}[DSDT]${OFF}: Patching SSDT-10 in ./DSDT/decompiled"	
 
 	echo "${BOLD}_BST package size${OFF}"
 	./tools/patchmatic ./DSDT/decompiled/SSDT-10.dsl ./DSDT/patches/_BST-package-size.txt ./DSDT/decompiled/SSDT-10.dsl
@@ -110,7 +120,7 @@ patch_dsdt()
 	# SSDT-12 Patches
 	########################
 
-	echo "${RED}Patching SSDT-12 in ./DSDT/decompiled${OFF}"	
+	echo "${BLUE}[DSDT]${OFF}: Patching SSDT-12 in ./DSDT/decompiled"	
 
 	echo "${BOLD}[gfx] Rename GFX0 to IGPU${OFF}"
 	./tools/patchmatic ./DSDT/decompiled/SSDT-12.dsl ./externals/Laptop-DSDT-Patch/graphics/graphics_Rename-GFX0.txt ./DSDT/decompiled/SSDT-12.dsl
@@ -131,7 +141,7 @@ patch_dsdt()
 	# SSDT-13 Patches
 	########################
 
-	echo "${RED}Patching SSDT-13 in ./DSDT/decompiled${OFF}"	
+	echo "${BLUE}[DSDT]${OFF}: Patching SSDT-13 in ./DSDT/decompiled"	
 
 	echo "${BOLD}[gfx] Rename GFX0 to IGPU${OFF}"
 	./tools/patchmatic ./DSDT/decompiled/SSDT-13.dsl ./externals/Laptop-DSDT-Patch/graphics/graphics_Rename-GFX0.txt ./DSDT/decompiled/SSDT-13.dsl
@@ -140,7 +150,7 @@ patch_dsdt()
 	# SSDT-15 Patches
 	########################
 
-	echo "${RED}Patching SSDT-15 in ./DSDT/decompiled${OFF}"	
+	echo "${BLUE}[DSDT]${OFF}: Patching SSDT-15 in ./DSDT/decompiled"	
 
 	echo "${BOLD}Remove invalid operands${OFF}"
 	./tools/patchmatic ./DSDT/decompiled/SSDT-15.dsl ./DSDT/patches/WMMX-invalid-operands.txt ./DSDT/decompiled/SSDT-15.dsl
@@ -154,7 +164,9 @@ patch_dsdt()
 
 patch_wifi()
 {
-	echo "${RED}Patching DSDT in ./DSDT/decompiled${OFF}"
+	echo "${GREEN}[WiFi]${OFF}: Applying WiFi patch to DSDT / SST"
+	
+	echo "${BLUE}[WiFi]${OFF}: Patching DSDT in ./DSDT/decompiled"
 
 	echo "${BOLD}Adding BCM94352Z Combo WiFi support${OFF}"
 	cd "${REPO}"
@@ -165,33 +177,33 @@ patch_wifi()
 
 compile_dsdt()
 {
-	echo "Compiling DSDT / SSDT in ./DSDT/compiled"
+	echo "${GREEN}[DSDT]${OFF}: Compiling DSDT / SSDT in ./DSDT/compiled"
 	cd "${REPO}"
 
 	rm ./DSDT/compiled/*
 
-	echo "${RED}Compiling DSDT to ./DSDT/compiled${OFF}"
+	echo "${BLUE}[DSDT]${OFF}: Compiling DSDT to ./DSDT/compiled"
 	./tools/iasl -vr -w1 -ve -p ./DSDT/compiled/DSDT.aml -I ./DSDT/decompiled/ ./DSDT/decompiled/DSDT.dsl
 
-	echo "${RED}Compiling SSDT-10 to ./DSDT/compiled${OFF}"
+	echo "${BLUE}[DSDT]${OFF}: Compiling SSDT-10 to ./DSDT/compiled"
 	./tools/iasl -vr -w1 -ve -p ./DSDT/compiled/SSDT-10.aml -I ./DSDT/decompiled/ ./DSDT/decompiled/SSDT-10.dsl
 
-	echo "${RED}Compiling SSDT-12 to ./DSDT/compiled${OFF}"
+	echo "${BLUE}[DSDT]${OFF}: Compiling SSDT-12 to ./DSDT/compiled"
 	./tools/iasl -vr -w1 -ve -p ./DSDT/compiled/SSDT-12.aml -I ./DSDT/decompiled/ ./DSDT/decompiled/SSDT-12.dsl
 
-	echo "${RED}Compiling SSDT-13 to ./DSDT/compiled${OFF}"
+	echo "${BLUE}[DSDT]${OFF}: Compiling SSDT-13 to ./DSDT/compiled"
 	./tools/iasl -vr -w1 -ve -p ./DSDT/compiled/SSDT-13.aml -I ./DSDT/decompiled/ ./DSDT/decompiled/SSDT-13.dsl
 
-	echo "${RED}Compiling SSDT-15 to ./DSDT/compiled${OFF}"
+	echo "${BLUE}[DSDT]${OFF}: Compiling SSDT-15 to ./DSDT/compiled"
 	./tools/iasl -vr -w1 -ve -p ./DSDT/compiled/SSDT-15.aml -I ./DSDT/decompiled/ ./DSDT/decompiled/SSDT-15.dsl
 
 	# Additional custom SSDT
 	# Rehabman NullEthernet.kext
-	echo "${RED}Compiling SSDT-rmne to ./DSDT/compiled${OFF}"
+	echo "${BLUE}[DSDT]${OFF}: Compiling SSDT-rmne to ./DSDT/compiled"
 	./tools/iasl -vr -w1 -ve -p ./DSDT/compiled/SSDT-16.aml ./DSDT/custom/SSDT-rmne.dsl
 
 	# ssdtPRgen (P-states / C-states)
-	echo "${RED}Compiling ssdtPRgen to ./DSDT/compiled${OFF}"
+	echo "${BLUE}[DSDT]${OFF}: Compiling ssdtPRgen to ./DSDT/compiled"
 	./tools/iasl -vr -w1 -ve -p ./DSDT/compiled/SSDT-17.aml ./DSDT/custom/SSDT-pr.dsl
 }
 
@@ -199,21 +211,22 @@ patch_iokit()
 {
 	iokit_md5=$(md5 -q "/System/Library/Frameworks/IOKit.framework/Versions/Current/IOKit")
 	
-	echo "${RED}Current IOKit md5 is${OFF} ${BOLD}${iokit_md5}${OFF}"
+	echo "${GREEN}[IOKit]${OFF}: Patching IOKit for maximum pixel clock"
+	echo "${BLUE}[IOKit]${OFF}: Current IOKit md5 is ${BOLD}${iokit_md5}${OFF}"
 	
 	case $iokit_md5 in
 		"2a8cbc2f6616d3f7a5e499bd2d5593ab")
-		echo "\t--> ${BOLD}Yosemite 10.10.1 IOKit (unpatched)${OFF}"
+		echo "         --> Yosemite 10.10.1 IOKit (${GREEN}unpatched${OFF})"
 		sudo perl -i.bak -pe 's|\xB8\x01\x00\x00\x00\xF6\xC1\x01\x0F\x85|\x33\xC0\x90\x90\x90\x90\x90\x90\x90\xE9|sg' /System/Library/Frameworks/IOKit.framework/Versions/Current/IOKit
 		sudo codesign -f -s - /System/Library/Frameworks/IOKit.framework/Versions/Current/IOKit
-		echo "\tPatched"
+		echo "         Patched"
 		;;
 		"8756e20f979c9e74c80f07b452ebfadd")
-		echo "\t--> ${BOLD}Yosemite 10.10.1 IOKit (patched, not signed)${OFF}"
+		echo "         --> Yosemite 10.10.1 IOKit (${RED}patched, not signed${OFF})"
 		;;
 		"f834136d72126cc9479604879270d24f")
-		echo " --> ${BOLD}Yosemite 10.10.1 IOKit (patched)${OFF}"
-		echo "IOKit is already patched, no action taken."
+		echo "         --> Yosemite 10.10.1 IOKit (${RED}patched${OFF})"
+		echo "         IOKit is already patched, no action taken."
 		;;
 	esac
 }
@@ -222,29 +235,30 @@ patch_opencl()
 {
 	opencl_md5=$(md5 -q "/System/Library/Frameworks/OpenCL.framework/Libraries/libCLVMIGILPlugin.dylib")
 	
-	echo "${RED}Current libCLVMIGILPlugin md5 is${OFF} ${BOLD}${opencl_md5}${OFF}"
+	echo "${GREEN}[OpenCL]${OFF}: Patching libCLVMIGILPlugin for OpenCL/OpenGL"	
+	echo "${BLUE}[OpenCL]${OFF}: Current libCLVMIGILPlugin md5 is ${BOLD}${opencl_md5}${OFF}"
 	
 	case $opencl_md5 in
 		"6d325a0b3785e82b8074fb8f64005fdd")
-		echo "\t--> ${BOLD}Yosemite 10.10.1 libCLVMIGILPlugin (unpatched)${OFF}"
+		echo "          --> Yosemite 10.10.1 libCLVMIGILPlugin (${GREEN}unpatched${OFF})"
 		sudo perl -i.bak -pe 's|([\xFF\xFC\x3D])\x86\x80\x12\x04|$1\x86\x80\x16\x04|sg' /System/Library/Frameworks/OpenCL.framework/Libraries/libCLVMIGILPlugin.dylib
 		echo "\tPatched"
 		;;
 		"a77fe21fa2cbf3958e7d43a9b9453535")
-		echo "${BOLD}Yosemite 10.10.1 libCLVMIGILPlugin (patched)${OFF}"
-		echo "libCLVMIGILPlugin is already patched, no action taken."
+		echo "          --> Yosemite 10.10.1 libCLVMIGILPlugin (${RED}patched${OFF})"
+		echo "          libCLVMIGILPlugin is already patched, no action taken."
 		;;
 	esac
 }
 
 patch_hda()
 {
-	echo "${RED}Creating AppleHDA injection kernel extension for${OFF} ${BOLD}ALC668${OFF}"
+	echo "${GREEN}[HDA]${OFF}: Creating AppleHDA injection kernel extension for ${BOLD}ALC668${OFF}"
 	cd "${REPO}"
 	
 	plist=./audio/AppleHDA_ALC668.kext/Contents/Info.plist
 	
-	echo "\t--> ${BOLD}Creating AppleHDA_ALC668 file layout${OFF}"
+	echo "       --> ${BOLD}Creating AppleHDA_ALC668 file layout${OFF}"
 	rm -R ./audio/AppleHDA_ALC668.kext 2&>/dev/null
 	
 	cp -R /System/Library/Extensions/AppleHDA.kext ./audio/AppleHDA_ALC668.kext
@@ -255,10 +269,10 @@ patch_hda()
 	rm ./audio/AppleHDA_ALC668.kext/Contents/version.plist
 	ln -s /System/Library/Extensions/AppleHDA.kext/Contents/MacOS/AppleHDA ./audio/AppleHDA_ALC668.kext/Contents/MacOS/AppleHDA
 
-	echo "\t--> ${BOLD}Copying AppleHDA_ALC668 audio platform & layout${OFF}"
+	echo "       --> ${BOLD}Copying AppleHDA_ALC668 audio platform & layout${OFF}"
 	cp ./audio/*.zlib ./audio/AppleHDA_ALC668.kext/Contents/Resources/
 
-	echo "\t--> ${BOLD}Configuring AppleHDA_ALC668 Info.plist${OFF}"
+	echo "       --> ${BOLD}Configuring AppleHDA_ALC668 Info.plist${OFF}"
 	replace=`/usr/libexec/plistbuddy -c "Print :NSHumanReadableCopyright" $plist | perl -pi -e 's/(\d*\.\d*)/9\1/'`
 	/usr/libexec/plistbuddy -c "Set :NSHumanReadableCopyright '$replace'" $plist
 	replace=`/usr/libexec/plistbuddy -c "Print :CFBundleGetInfoString" $plist | perl -pi -e 's/(\d*\.\d*)/9\1/'`
@@ -277,9 +291,9 @@ patch_hda()
 	/usr/libexec/plistbuddy -c "Set ':IOKitPersonalities:HDA Hardware Config Resource:IOProbeScore' 2000" $plist
 	/usr/libexec/plistbuddy -c "Merge ./audio/ahhcd.plist ':IOKitPersonalities:HDA Hardware Config Resource'" $plist
     
-	echo "\t--> ${BOLD}Created AppleHDA_ALC668.kext${OFF}"
-	sudo cp -r ./audio/AppleHDA_ALC668.kext /System/Library/Extensions
-	echo "\t--> ${BOLD}Installed AppleHDA_ALC668.kext${OFF}"
+	echo "       --> ${BOLD}Created AppleHDA_ALC668.kext${OFF}"
+	#sudo cp -r ./audio/AppleHDA_ALC668.kext /System/Library/Extensions
+	echo "       --> ${BOLD}Installed AppleHDA_ALC668.kext to /System/Library/Extensions${OFF}"
 }
 
 RETVAL=0
@@ -299,9 +313,9 @@ case "$1" in
 		echo "\t${BOLD}--patch-hda${OFF}: Create AppleHDA injector kernel extension"
 		echo
 		echo "Credits:"
-		echo "Laptop-DSDT: https://github.com/RehabMan/Laptop-DSDT-Patch"
-		echo "ssdtPRgen: https://github.com/Piker-Alpha/ssdtPRGen.sh"
-		echo "AppleHDA ALC668: https://github.com/vbourachot/Dell-XPS13-9333-DSDT-Patch/"
+		echo "${BLUE}Laptop-DSDT${OFF}: https://github.com/RehabMan/Laptop-DSDT-Patch"
+		echo "${BLUE}ssdtPRgen${OFF}: https://github.com/Piker-Alpha/ssdtPRGen.sh"
+		echo "${BLUE}AppleHDA ALC668${OFF}: https://github.com/vbourachot/Dell-XPS13-9333-DSDT-Patch/"
 		echo
 		RETVAL=1
     ;;

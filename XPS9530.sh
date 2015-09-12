0c2a5edb5aa99dbdd7aa9a48f74c0750eb8e9888
the-darkvoid <the-darkvoid@users.noreply.github.com>
2015-09-12 09:57:27 +0400

    Updated IOKit for Yosemite 10.10.5

--- XPS9530.sh
+++ XPS9530.sh
@@ -240,7 +240,7 @@ patch_iokit()
 		echo "         Patched"
 		;;
 		"29d7632362b2fa4993156717671a5642")
-		echo "         --> Yosemite 10.10.3 / 10.10.4 IOKit (${GREEN}unpatched${OFF})"
+		echo "         --> Yosemite 10.10.3 / 10.10.4 / 10.10.5 IOKit (${GREEN}unpatched${OFF})"
 		sudo perl -i.bak -pe 's|\xB8\x01\x00\x00\x00\xF6\xC1\x01\x0F\x85|\x33\xC0\x90\x90\x90\x90\x90\x90\x90\xE9|sg' /System/Library/Frameworks/IOKit.framework/Versions/Current/IOKit
 		sudo codesign -f -s - /System/Library/Frameworks/IOKit.framework/Versions/Current/IOKit
 		echo "         Patched"
@@ -270,7 +270,7 @@ patch_iokit()
 		echo "         --> Yosemite 10.10.2 IOKit (${RED}patched, not signed${OFF})"
 		;;
 		"4bd81492fd13e905ef10719ef391e8a0")
-		echo "         --> Yosemite 10.10.3 / 10.10.4 IOKit (${RED}patched, not signed${OFF})"
+		echo "         --> Yosemite 10.10.3 / 10.10.4 / 10.10.5 IOKit (${RED}patched, not signed${OFF})"
 		;;
 		"1ab7c0ec047d11f6b40798b6f0107c0c")
 		echo "         --> El Capitan 10.11 Beta 1 IOKit (${RED}patched, not signed${OFF})"
@@ -290,7 +290,7 @@ patch_iokit()
 		echo "         IOKit is already patched, no action taken."
 		;;
 		"a045c1ac523fece1f1b083b2c5ee842c"|"4c99100b36f37df4bdcc5dc4cd2b8237")
-		echo "         --> Yosemite 10.10.3 / 10.10.4 IOKit (${RED}patched${OFF})"
+		echo "         --> Yosemite 10.10.3 / 10.10.4 / 10.10.5 IOKit (${RED}patched${OFF})"
 		echo "         IOKit is already patched, no action taken."
 		;;
 		"e96a04420555b71bb0933e26773575bc")
